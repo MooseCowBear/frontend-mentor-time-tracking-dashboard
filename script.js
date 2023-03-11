4a34846cbb0967b1c7d05afb4c72ec84e0562f4a
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-03-11 15:46:55 -0500

    Remove unused function

--- script.js
+++ script.js
@@ -2,14 +2,6 @@
 let selection = "weekly"; //default
 const prevParagraph = {weekly: "Last Week - ", daily: "Yesterday - ", monthly: "Last month - "};
 
-
- /*let data = fetch("https://raw.githubusercontent.com/MooseCowBear/frontend-mentor-time-tracking-dashboard/main/data.json")
- .then((response) => response.json())
- .catch(error => displayError(error));
-
- data.then(result => displayData(result));
- data.then(result => addButtonListeners(result)); //can i do this?  */
-
 async function getData() {
   const dataUrl = "https://raw.githubusercontent.com/MooseCowBear/frontend-mentor-time-tracking-dashboard/main/data.json";
   try {
@@ -31,74 +23,7 @@ async function updateDOM() {
   addButtonListeners(categories);
 }
 
-//call update dom 
 updateDOM();
-
-
-/* for each element in the data arr, going to create a section + a bunch of children to go in it,
- and append it to main*/
-function displayData(result) { //rewrite me!
-  const parent = document.getElementById("main");
-
-  for (const elem of result) {
-    const section = document.createElement("section");
-    section.classList.add("category");
-    const name = getName(elem);
-    section.classList.add(name); 
-
-    const imgUrl = `./images/icon-${name}.svg`;
-    const image = document.createElement("img");
-    image.setAttribute("src", imgUrl);
-    image.setAttribute("alt", `${name} icon`);
-
-    section.appendChild(image);
-
-    const titleDiv = document.createElement("div");
-    titleDiv.classList.add("section-heading");
-
-    const title = document.createElement("h2");
-    title.innerText = elem.title; 
-
-    const ellipsis = document.createElement("img");
-    ellipsis.setAttribute("src", "./images/icon-ellipsis.svg");
-    ellipsis.setAttribute("alt", "ellipsis");
-
-    titleDiv.appendChild(title);
-    titleDiv.appendChild(ellipsis);
-
-    const sectionInfo = document.createElement("div");
-    sectionInfo.classList.add("section-info");
-    sectionInfo.setAttribute("tabindex", 0);
-
-    const paragraphDiv = document.createElement("div");
-    paragraphDiv.classList.add("paragraph-wrapper");
-
-    const pCurr = document.createElement("p");
-    pCurr.classList.add("curr");
-
-    pCurr.innerText = `${elem.timeframes[selection].current}hrs`; //
-
-    const pPrev = document.createElement("p");
-    pPrev.classList.add("prev");
-
-    pPrev.innerText = prevParagraph[selection]; //
-
-    const theSpan = document.createElement("span");
-
-    theSpan.innerText = `${elem.timeframes[selection].previous}hrs`; //
-
-    pPrev.appendChild(span); //now not working, whereas before it had
-
-    paragraphDiv.appendChild(pCurr);
-    paragraphDiv.appendChild(pPrev);
-
-    sectionInfo.appendChild(titleDiv);
-    sectionInfo.appendChild(paragraphDiv);
-
-    section.appendChild(sectionInfo);
-    parent.appendChild(section);
-  }
-}
 
 function createDisplayElements(data) {
   const parent = document.getElementById("main");
